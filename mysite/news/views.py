fff7354300ed566e5c9b4572fd69137b31b24ee5
kznar <kznartee16reg@gmail.com>
2021-04-02 15:24:24 +0300

    добавление рендеринга шаблона

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,9 +1,12 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from .models import News
+
+
 def index(request):
-    return HttpResponse('Здарова нахуй')
+    news = News.objects.order_by('-created_at')
+    return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
 
 
-def test(request):
-    return HttpResponse('<h1> Sup bro </h1>')
