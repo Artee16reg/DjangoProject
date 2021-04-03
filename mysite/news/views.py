2edd62dc2645dd65bea26a3c55a32465a35b6dea
kznar <kznartee16reg@gmail.com>
2021-04-03 10:28:21 +0300

    настройка модели в админке

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 from .models import News
 
 
 def index(request):
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
-
-
